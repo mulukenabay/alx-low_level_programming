cb080622d07fc644449145c0afa400149002505c
mulukenkebede <eliyas.muluken@gmail.com>
2022-06-30 13:20:45 -0700

    good

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,10 +1,11 @@
 #include <stdio.h>
+#include <unistd.h>  
 /**
  * main - Entry point
  * Return: Always 0 (Success)
  */
 int main(void)
 {
-write(2, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 61);
-return (1);
+printf("with proper grammar, but the outcome is a piece of art,\n");
+return (0);
 }
