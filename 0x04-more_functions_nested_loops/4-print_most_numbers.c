a71531ee764f138217b6e1a1acc1ed0af37e1b6a
mulukenkebede <eliyas.muluken@gmail.com>
2022-07-07 06:37:14 -0700

    4-print_most_numbers.c

--- 0x04-more_functions_nested_loops/4-print_most_numbers.c
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -6,15 +6,10 @@
 void print_most_numbers(void)
 {
 int a;
-for (a = 48; a < 58; a++)
+for (a = 0; a < 10; a++)
 {
-if (a != 50)
-{
-if (a != 52)
-{
-_putchar(a);
+if (a != 2 && a != 4)
+_putchar(x);
 }
-}
-}
-putchar('\n');
+_putchar(\n);
 }
