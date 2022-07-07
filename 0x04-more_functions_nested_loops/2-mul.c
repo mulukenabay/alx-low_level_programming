11afae96b872f4d651314825385f929ff2c7873b
mulukenkebede <eliyas.muluken@gmail.com>
2022-07-07 05:20:28 -0700

    2-mul.c

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,12 @@
+#include "main.h"
+/**
+ * mul - multiplies two integeres
+ * @a: a -  Variable
+ * @b: b -  Variable
+ * Return: intiger
+ */
+int mul(int a, int b);
+{
+int result = a * b;
+return (result);
+}
