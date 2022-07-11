4ec14d3887a23a828858f33465bb7f23eeff6921
mulukenkebede <eliyas.muluken@gmail.com>
2022-07-11 06:03:50 -0700

    ff

--- 0x05-pointers_arrays_strings/8-print_array.c
+++ 0x05-pointers_arrays_strings/8-print_array.c
@@ -9,8 +9,19 @@
 void print_array(int *a, int n)
 {
 int i;
-for (i = 0; i < (n - 1); i++)
+if (n > 0)
+{
+for (i = 0 ; n > i; i++)
+{
+if (i != n - 1)
+{
 printf("%d, ", a[i]);
-printf("%d", a[n - 1]);
+}
+else
+{
+printf("%d", a[i]);
+}
+}
+}	
 printf("\n");
 }
