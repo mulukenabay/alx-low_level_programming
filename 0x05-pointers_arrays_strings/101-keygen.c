6decc177053f37a9c659290f9a0a01b6ff7f9891
mulukenkebede <eliyas.muluken@gmail.com>
2022-07-11 12:17:51 -0700

    last

--- 0x05-pointers_arrays_strings/101-keygen.c
+++ 0x05-pointers_arrays_strings/101-keygen.c
@@ -1,1 +1,20 @@
-
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+/**
+ * main - randem password
+ * Return: 0 
+ */
+int main()
+{
+int pass, sum;
+sum = 0;
+while (sum <= 2645)
+{
+pass = (rand() % 128);
+sum += pass;
+printf("%c", pass);
+}
+printf("%c", 2772 - sum);
+return (0);
+}
