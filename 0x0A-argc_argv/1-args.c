1db5778c6fc227653d5a4f09231ff7fdc85db08c
mulukenkebede <eliyas.muluken@gmail.com>
2022-07-22 04:01:30 -0700

    modification

--- 0x0A-argc_argv/1-args.c
+++ 0x0A-argc_argv/1-args.c
@@ -8,13 +8,7 @@
  */
 int main(int argc, char *argv[])
 {
-int i;
-if (argc == 1)
+(void) argv;
 printf("%d\n", argc - 1);
-else
-{
-for (i = 0; *argv; i++, argv++);
-printf("%d\n", i - 1);
-}
 return (0);
 }
