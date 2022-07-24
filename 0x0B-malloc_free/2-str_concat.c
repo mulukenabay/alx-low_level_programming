6366da2bafd5d16825f6c8e9aab26ec5b95fabc8
mulukenkebede <eliyas.muluken@gmail.com>
2022-07-24 09:12:52 -0700

    2-str_concat.c

--- 0x0B-malloc_free/2-str_concat.c
+++ 0x0B-malloc_free/2-str_concat.c
@@ -8,33 +8,43 @@
  * Return: pointer should point to a newly allocated space in memory or NULL
  */
 char *str_concat(char *s1, char *s2)
+
 {
-char *strnew = NULL;
-unsigned int i;
-int n1;
-int n2;
-int count;
-count = 0;
-if (s1 == NULL)
-s1 = "";
-if (s2 == NULL)
-s2 = "";
-for (n1 = 0; s1[n1] != '\0'; n1++)
-;
-for (n2 = 0; s2[n2] != '\0'; n2++)
-;
-strnew = (char *)malloc((n1 + n2 + 1) * sizeof(char));
-if (strnew == NULL)
-{
-return (NULL);
-for (i = 0; s1[i] != '\0'; i++)
-strnew[i] = s1[i];
-for (; s2[count] != '\0'; i++)
-{
-strnew[i] = s2[count];
-count++;
-}
-return (strnew);
-}
-return (0);
+	int i, j, k, e, coun;
+	int coun1 = 0;
+	int coun2 = 0;
+	char *q;
+
+	if (s1 == NULL)
+	{
+		s1 = "";
+	}
+	if (s2 == NULL)
+	{
+		s2 = "";
+	}
+	for (i = 0; s1[i] != '\0'; i++)
+	{
+		coun1 = coun1 + 1;
+	}
+	for (j = 0; s2[j] != '\0'; j++)
+	{
+		coun2 = coun2 + 1;
+	}
+	coun = coun1 + coun2;
+	q = malloc(sizeof(char) * coun + 1);
+	if (q == NULL)
+	{
+		return (NULL);
+	}
+	for (k = 0; s1[k] != '\0'; k++)
+	{
+		q[k] = s1[k];
+	}
+	for (k = k, e = 0; s2[e] != '\0'; e++, k++)
+	{
+		q[k] = s2[e];
+	}
+	q[k] = '\0';
+	return (q);
 }
