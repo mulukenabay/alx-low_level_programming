9cd32f47a50a92698a9ab77423953c2086eab9ff
mulukenkebede <eliyas.muluken@gmail.com>
2022-07-24 09:21:10 -0700

    1-strdup.c

--- 0x0B-malloc_free/1-strdup.c
+++ 0x0B-malloc_free/1-strdup.c
@@ -9,23 +9,27 @@
  */
 char *_strdup(char *str)
 {
-char *strnew = NULL;
-unsigned int i;
-int n;
+	char *r;
+	int i, j;
+	int coun = 0;
 
-if (str == NULL)
-return (NULL);
-for (n = 0; str[n] != '\0'; n++)
-strnew = (char *)malloc(n + 1 * sizeof(char));
-if (strnew != NULL)
-{
-for (i = 0; str[i] != '\0'; i++)
-strnew[i] = str[i];
-}
-else
-{
-return (NULL);
-}
-strnew[i] = '\0';
-return (strnew);
+	if (str == NULL)
+	{
+		return (NULL);
+	}
+	for (i = 0; str[i] != '\0'; i++)
+	{
+		coun = coun + 1;
+	}
+	r = malloc(sizeof(char) * coun + 1);
+	if (r == NULL)
+	{
+		return (NULL);
+	}
+	for (j = 0; str[j] != '\0' ; j++)
+	{
+		r[j] = str[j];
+	}
+	r[j] = '\0';
+	return (r);
 }
