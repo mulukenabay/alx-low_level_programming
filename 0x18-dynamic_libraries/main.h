c5478450e4cc36e373c3def054cda3869ee0eb4a
mulukenabay <mule.andisew@gmail.com>
2022-10-03 09:29:00 -0700

    dynamic lib

--- 0x18-dynamic_libraries/main.h
+++ 0x18-dynamic_libraries/main.h
@@ -1,8 +1,5 @@
 #ifndef "MAIN_H"
 #define "MAIN_H"
-#include <stdio.h>
-#include <stdlib.h>
-
 int _putchar(char c);
 int _islower(int c);
 int _isalpha(int c);
@@ -10,7 +7,7 @@ int _abs(int n);
 int _isupper(int c);
 int _isdigit(int c);
 int _strlen(char *s);
-void _puts(char *str);
+void _puts(char *s);
 char *_strcpy(char *dest, char *src);
 int _atoi(char *s);
 char *_strcat(char *dest, char *src);
