a408ec576b345b7d82cf89614cd225be5c564587
mulukenabay <106770288+mulukenabay@users.noreply.github.com>
2022-10-04 02:16:58 +0300

    Update 101-make_me_win.sh
    change 101-make-me file

--- 0x18-dynamic_libraries/101-make_me_win.sh
+++ 0x18-dynamic_libraries/101-make_me_win.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-wget -P /tmp https://github.com/mulukenabay/alx-low_level_programming/raw/master/0x18-dynamic_libraries/nrandom.so
-export LD_PRELOAD=/tmp/nrandom.so
+gcc *.o -shared -o nrandom.so
+export LD_PRELOAD=$PWD/nrandom.so
